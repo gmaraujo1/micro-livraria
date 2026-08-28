e2359ccdbbe3fea9c24e5320b924a4df0f5b600b
gustavo <gmaraujo1@minha.fag.edu.br>
2026-08-28 19:44:19 -0300

    Tarefa prática #1 - Microservices

--- services/inventory/index.js
+++ services/inventory/index.js
@@ -20,6 +20,14 @@ server.addService(inventoryProto.InventoryService.service, {
             products: products,
         });
     },
+
+    SearchProductByID: (payload, callback) => {
+        const product = products.find((product) => product.id == payload.request.id);
+
+        callback(null, {
+            products: product ? [product] : [],
+        });
+    },
 });
 
 server.bindAsync('127.0.0.1:3002', grpc.ServerCredentials.createInsecure(), () => {
